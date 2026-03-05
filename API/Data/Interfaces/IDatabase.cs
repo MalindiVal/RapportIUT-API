48ac37bee4950be3fc2f3dc9220c9702e56c80aa
MalindiVal <63977034+MalindiVal@users.noreply.github.com>
2026-03-05 10:23:12 +0100

    Interface Database

---
+++ API/Data/Interfaces/IDatabase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace API.Data.Interfaces
+{
+    public interface IDatabase
+    {
+        public DataTable ExecuteQuery(string query, Dictionary<string, object> parameters = null);
+
+        /// <summary>
+        /// Execute un insert et renvoie l'id de celui-ci
+        /// </summary>
+        /// <param name="query">La requête d'insert</param>
+        /// <param name="parameters">Le dictionnaire des paramètres</param>
+        /// <returns>L'id de la ligne inséré</returns>
+        public long ExecuteInsert(string query, Dictionary<string, object> parameters = null);
+    }
+}
